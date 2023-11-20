8d7491af093060cac4444b13f77d13a474e58549
kkostorz <53268565+kkostorz@users.noreply.github.com>
2023-11-20 18:28:42 +0100

    Update RPS_windows_detectability_glme.m

--- RPS/RPS_windows_detectability_glme.m
+++ RPS/RPS_windows_detectability_glme.m
@@ -12,7 +12,7 @@
 % KK last version Oct. 2023
 
 
-path    = ['Your/Path/To/Data'];
+path    = ['Your/Path/To/Data']; %Unix System, change accordingly
 
 pairlist   = {'01','02','03','04','05','06','08','09','10','11','13', ...
     '14','15','17','18','19','20','21','22','23','24','26','28','29', ...
